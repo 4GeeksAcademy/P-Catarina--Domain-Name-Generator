5bda53dbdffb177a93af4a76ed58e5a0bf8b5456
P-Catarina <155997817+P-Catarina@users.noreply.github.com>
2024-02-15 23:48:50 +0000

    domain generator project

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
 let extensions = ['.com', '.net', '.us', '.io'];
 
-
 let generator = "";
 
+//Method 1
 for (a of pronoun){
     for (b of adj){
         for (c of noun){
             for (d of extensions){
-            console.log(generator = a + b + c + d)
+            console.log(generator = /*'Method 1: ' +*/ a + b + c + d)
             }
         }
     }
 }
+
+//Method 2
+pronoun.forEach(a => adj.forEach(b => noun.forEach(c => extensions.forEach(d => console.log(/*'Method 2: '+*/ a+b+c+d)))))
